f89ede0106bf4d1882b3f40573ff22338aa25b35
Robert Brown <rabrown.mail@gmail.com>
2013-07-26 10:10:46 -0400

    Adding email sending support.

--- monitor.rb
+++ monitor.rb
@@ -1,8 +1,29 @@
 require 'mysql'
+require 'mail'
+require 'erb'
 
 # ADD DB CREDENTIALS
-db = Mysql.new('localhost', 'root', '', 'information_schema')
+db_host = 'localhost'
+db_user = 'root'
+db_pw = ''
+db_db = 'information_schema'
 
+# SMTP CREDENTIALS
+options = { :address              => "smtp.gmail.com",
+            :port                 => 587,
+            :domain               => 'domainname',
+            :user_name            => 'username',
+            :password             => 'password',
+            :authentication       => 'plain',
+            :enable_starttls_auto => true  }
+
+# Comparison Threshold
+threshold = 1.2
+
+
+db = Mysql.new(db_host, db_user, db_pw, db_db)
+
+# Wrap sql calls in block to ensure connection to db is closed.
 begin
   # GET RECORDS FROM information_schema db
   info_results = db.query "SELECT table_schema AS db_name,
@@ -19,10 +40,9 @@ begin
 
   # ONCE DONE COMPARING, UPDATE current_size RECORDS WITH information_schema RESULTS
   info_results.each_hash do |row|
-    puts "Table #{row['db_name']}: #{row['tbl_name']} (#{row['num_rows']})"
     old_results.each_hash do |old|
       if row['db_name'] == old['db_name'] && row['tbl_name'] == old['tbl_name']
-        if row['num_rows'].to_f >= 1.2 * old['num_rows'].to_f
+        if row['num_rows'].to_f >= threshold * old['num_rows'].to_f
           size_issues << {
             db_name: row['db_name'],
             tbl_name: row['tbl_name'],
@@ -44,3 +64,18 @@ ensure
 end
 
 # EMAIL OUT LIST size_issues
+unless size_issues.empty?
+  email_template = ERB.new(File.read('_mail.html.erb'))
+
+  Mail.deliver do
+    delivery_method :smtp, options
+    to      'toemail'
+    from    'fromemail'
+    subject 'MySQL tables growing faster than normal'
+
+    html_part do
+      content_type 'text/html; charset=UTF-8'
+      body email_template.result(binding)
+    end
+  end
+end
